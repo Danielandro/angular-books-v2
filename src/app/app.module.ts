0aa6acb3f4b4779e0a59e728e1b6adc7b2e6f238
Danny <mtme13@hotmail.com>
2020-02-05 17:22:19 +0000

    chore: Add routing for books and detail pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,33 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
 
 import { AppComponent } from "./app.component";
 import { BookListComponent } from "./feature/book/pages/book-list/book-list.component";
 import { BookCardComponent } from "./feature/book/components/book-card/book-card.component";
 import { BookSearchComponent } from "./feature/book/components/book-search/book-search.component";
+import { BookDetailComponent } from "./feature/book/pages/book-detail/book-detail.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     BookListComponent,
     BookCardComponent,
-    BookSearchComponent
+    BookSearchComponent,
+    BookDetailComponent
   ],
-  imports: [BrowserModule, CommonModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: "books", component: BookListComponent },
+      { path: "books/detail/:id", component: BookDetailComponent },
+      { path: "", redirectTo: "books", pathMatch: "full" },
+      { path: "**", component: BookListComponent }
+    ])
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/feature/book/pages/book-detail/book-detail.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './book-detail.component.html',
+  styleUrls: ['./book-detail.component.scss']
+})
+export class BookDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
